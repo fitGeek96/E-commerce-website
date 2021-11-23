5b032751f242ede3a177d4ef4a8747a365a093a5
fitgeek96 <fitgeek96@gmail.com>
2021-11-23 17:43:19 +0200

    broken our homepage into smaller components, finished styling our menu-item component

--- src/components/menu-item/menu-item.js
+++ src/components/menu-item/menu-item.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './menu-item.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div style={{
-        backgroundImage: `url(${imageUrl})`
-    }} className={`${size} menu-item`}>
-    <div className="content">
-        <h1 className="title">{ title }</h1>
-        <span className="subtitle">Shop Now</span>
+const MenuItem = ({title, imageUrl, size}) => (
+    <div className={`${size} menu-item`}>
+        <div
+            className='background-image'
+            style={{
+            backgroundImage: `url(${imageUrl})`
+        }}/>
+        <div className="content">
+            <h1 className="title">{title.toUpperCase()}</h1>
+            <span className="subtitle">Shop Now</span>
+        </div>
     </div>
-</div>
 );
-
 
 export default MenuItem;
