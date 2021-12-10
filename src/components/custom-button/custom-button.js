7018e118512eced9d3d1f088071b01fe4a7b2a27
fitgeek96 <fitgeek96@gmail.com>
2021-12-10 20:56:48 +0200

    adding mutliple items to Cart

--- src/components/custom-button/custom-button.js
+++ src/components/custom-button/custom-button.js
@@ -4,12 +4,16 @@ import './custom-button.styles.scss'
 const CustomButton = ({
     children,
     isGoogleSignIn,
+    inverted,
     ...otherProps
 }) => (
     <button
-        className={`${isGoogleSignIn
-        ? 'google-sign-in'
-        : ''} custom-button`}
+        className={`${inverted
+        ? 'inverted'
+        : ''}
+        ${isGoogleSignIn
+            ? 'google-sign-in'
+            : ''} custom-button`}
         { ...otherProps }>
         {children}
     </button>
--- src/components/preview-collection/preview-collection.js
+++ src/components/preview-collection/preview-collection.js
@@ -6,10 +6,9 @@ const PreviewCollection = ({title, items}) => (
     <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
-            {items.filter((item, index) => index < 4).map(({
-                id,
-                ...otherItemPros
-            }) => (<CollectionItem key={id} { ...otherItemPros }/>))
+            {items
+            .filter((item, index) => index < 4)
+            .map(({item}) => (<CollectionItem key={item.id} item={item} />))
 }
         </div>
     </div>
